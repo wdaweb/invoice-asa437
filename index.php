093a22101875703c8c34c36f4ed4aea45e3e6c75
unknown <klk437@yaho.com.tw>
2020-05-08 14:45:56 +0800

    建立輸入發票的畫面及流程

--- index.php
+++ index.php
@@ -7,6 +7,32 @@
     
 </head>
 <body>
-    <?php include "./include/header.php";?>
+<?php include "./include/header.php"  ;?>
+
+<form action="save_invoice.php" method="post">
+    期別:
+    <select name="period" id="">
+        <option value="1">1,2月</option>
+        <option value="2">3,4月</option>
+        <option value="3">5,6月</option>
+        <option value="4">7,8月</option>
+        <option value="5">9,10月</option>
+        <option value="6">11,12月</option>
+    </select>
+    年份:
+    <select name="year" id="">
+        <option value="2020">2020</option>
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+    </select>
+    獎號:
+    <input type="text" name="code" id="">
+    <input type="number" name="number" id="">
+    花費:
+    <input type="number" name="spend" id="">
+    <input type="submit" value="儲存">
+
+
+</form>
 </body>
 </html>
